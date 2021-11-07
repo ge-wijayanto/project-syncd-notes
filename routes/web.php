362cb3de4b2627340c46105b4c43c0ae9a4a145b
lalislis <realihsan02@gmail.com>
2021-11-07 21:53:03 +0700

    Adding AuthController for login and register

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,12 @@ Route::get('/', function() {
     return view('landing');
 });
 
-Route::get('/login', function() {
-    return view('login');
-})->name('login');
+// Route::get('/login', function() {
+//     return view('login');
+// })->name('login');
+
+Route::get('/login', 'CustomAuthController@index')->name('login');
+Route::post('custom-login', 'CustomAuthController@customLogin')->name('login.custom'); 
+Route::get('/register', 'CustomAuthController@registration')->name('register-user');
+Route::post('custom-registration', 'CustomAuthController@customRegistration')->name('register.custom');
+Route::get('signout', 'CustomAuthController@signOut')->name('signout');
